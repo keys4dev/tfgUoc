5cc9fbb8cf128c4ca321626c521e79b1a461bc1a
Keys 4 Dev <75089820+keys4dev@users.noreply.github.com>
2022-11-07 19:53:07 +0100

    Add files via upload

---
+++ tfgUoc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tfgUoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
